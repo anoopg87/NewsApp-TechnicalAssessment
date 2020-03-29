4b3fd1aedd203ab88c7b56199237384c7e8e580b
agopalakrishnan <agopalakrishnan@freelancer.com>
2020-03-30 04:08:32 +1100

    Covers the local storage condition in unit test

--- app/src/test/kotlin/com/anooplab/newsapp/factslist/FactsListViewModelTest.kt
+++ app/src/test/kotlin/com/anooplab/newsapp/factslist/FactsListViewModelTest.kt
@@ -1,12 +1,14 @@
 package com.anooplab.newsapp.factslist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
 import com.anooplab.business.model.FactItemBusinessModel
 import com.anooplab.business.model.FactsBusinessModel
 import com.anooplab.business.usecase.IGetFactsUseCase
 import com.anooplab.newsapp.factslist.model.mapToUiModel
 import com.anooplab.newsapp.util.IConnectionManager
+import com.nhaarman.mockitokotlin2.never
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Single
 import org.junit.Assert.assertEquals
@@ -36,9 +38,7 @@ class FactsListViewModelTest {
     }
 
     @Test
-    fun `User should see the lists of facts on successful fetch of data`() {
-        val observer: Observer<Boolean> = com.anooplab.newsapp.mock()
-        viewModel.loadingObserver.observeForever(observer)
+    fun `User should see the lists of facts from network for the first showFacts call`() {
 
         val factsBusinessModel = FactsBusinessModel().apply {
             title = "Test"
@@ -56,11 +56,33 @@ class FactsListViewModelTest {
     }
 
     @Test
+    fun `User should see the lists of facts from local storage on second call to showFacts`() {
+
+        val factsBusinessModel = FactsBusinessModel().apply {
+            title = "Test"
+            factItemBusinessModelList =
+                listOf(FactItemBusinessModel(), FactItemBusinessModel(), FactItemBusinessModel())
+        }
+
+        whenever(connectionManager.isConnectedToInternet()).thenReturn(true)
+        whenever(getFactsUseCase()).thenReturn(Single.just(factsBusinessModel))
+
+        viewModel.showFacts()
+        viewModel.showFacts()
+
+        verify(getFactsUseCase, times(1)).invoke()
+        assertEquals(viewModel.loadingObserver.value, false)
+        assertEquals(viewModel.factsObserver.value, factsBusinessModel.mapToUiModel())
+    }
+
+    @Test
     fun `Should show error message on no internet connection available`() {
+
         whenever(connectionManager.isConnectedToInternet()).thenReturn(false)
 
         viewModel.showFacts()
 
+        verify(getFactsUseCase, never()).invoke()
         assertEquals(viewModel.errorObserver.value, "Please check the internet connection")
     }
 }
